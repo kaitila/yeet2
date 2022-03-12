41611af277aa63b1bd80022127af6533b2289ef9
kaitila <arttu.kaitila@gmail.com>
2022-03-12 23:47:57 +0200

    Cleaned up old code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from cgitb import handler
 import discord
 from discord.ext import commands
 from pytube import Search, YouTube
@@ -11,94 +10,9 @@ import asyncio
 import os
 import re
 
+
 os.chdir('songs')
 load_dotenv()
-
-
-#Handles audio actions
-#class Audio():
-#    def __init__(self, bot, ctx):
-#        self.bot = bot
-#        self.audio_player = bot.loop.create_task(self.audio_player())
-#        self.queue = []
-#        self.ctx = ctx
-#
-#    #Automatically plays audio from the queue
-#    async def audio_player(self):
-#        while True:
-#            await asyncio.sleep(1)
-#            if len(self.queue) > 0:
-#                try:
-#                    await self.ctx.author.voice.channel.connect()
-#                    #self.voice = discord.utils.get(self.bot.voice_clients, guild=self.ctx.guild)
-#                except: pass
-#
-#                self.voice = discord.utils.get(self.bot.voice_clients, guild=self.ctx.guild)
-#
-#                if not self.voice.is_playing():
-#                    self.voice.play(discord.FFmpegPCMAudio(f"{self.queue[0]}.mp3"))
-#                    await self.ctx.send(f'`Now playing: {self.queue[0]}`')
-#                    self.queue.pop()
-#
-#
-#        
-#    def add_to_queue(self, title, ctx):
-#        #try: self.queue[ctx.guild.id]
-#        #except KeyError: self.queue[ctx.guild.id] = []
-#
-#        self.queue.append(title)
-#        print(self.queue)     
-#
-#    def search(self, search):
-#        yt_search = Search(search)
-#        id = re.split('videoId=', str(yt_search.results[0]))[1]
-#
-#        link = f'https://www.youtube.com/watch?v={id}'
-#        video = YouTube(link)
-#
-#        title = video.title
-#        regex = '[/\:?*|"]'
-#        title = re.sub(regex, "", title)
-#
-#        return title, link
-#
-#    def download(self, link, title):
-#        video = YouTube(link)
-#        streams = video.streams.filter(only_audio=True, bitrate="128kbps")
-#        streams[0].download(filename=f"{title}.mp3")
-#
-#    #Return a search from song library \songs
-#    def lib_search(self, search):
-#        search = search.lower()
-#        result = [0, ""]
-#
-#        for song in os.listdir():
-#            #Get filename and check for matching words
-#            matches = re.findall(search, str(re.sub(".mp3", "", song)).lower())
-#
-#            if len(matches) > result[0]:
-#                result = [len(matches), str(re.sub(".mp3", "", song))]
-#            elif len(matches) == result[0] and len(str(re.sub(".mp3", "", song))) < len(result[1]):
-#                result = [len(matches), str(re.sub(".mp3", "", song))]
-#
-#        if result[1] == '': return False
-#        return result[1]
-#
-#    def skip(self):
-#        try:
-#            self.voice.stop()
-#            return '`Skipping the current song!`'
-#        except:
-#            return '`Nothing is playing!`'
-#
-#    def get_queue(self):
-#        if self.queue == []: return '`Nothing in queue`'
-#
-#        message = ''
-#        for i in range(0, len(self.queue)):
-#            message += f'{i + 1}. {self.queue[i]}\n'
-#        
-#        return message
 
 
 #Main command handler
